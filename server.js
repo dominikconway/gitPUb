6aec009f1a8d6f27cbab0192c5f90d8d6ff65e61
Dominik Conway <dominik.conway@gmail.com>
2022-04-07 21:28:25 -0700

    adds show route set up

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
 const drinks = require('./models/drinks.js')
@@ -10,6 +11,11 @@ app.get('/', (req, res) => {
 app.get('/drinks', (req, res) => {
     res.render('index.ejs', {allDrinks: drinks})
 })
+
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
 
 
 app.listen(port, () => {
