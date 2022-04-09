9e2f0adc30c3274bc6bcd8cdff85d2635d6b48e1
Dominik Conway <dominik.conway@gmail.com>
2022-04-08 17:02:50 -0700

    adds food menu

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const res = require('express/lib/response')
 const app = express()
 const port = 3000
 const drinks = require('./models/drinks.js')
+const foods = require('./models/food.js')
 
 // welcome page
 app.get('/', (req, res) => {
@@ -17,6 +18,13 @@ app.get('/drinks/:id', (req, res) => {
     res.render('show.ejs', {drink: drinks[req.params.id]})
 })
 
+app.get('/foods', (req, res) => {
+    res.render('food_index.ejs',{allFoods: foods})
+})
+
+app.get('/foods/:id', (req, res) => {
+    res.render('food_show.ejs',{food: foods[req.params.id]})
+})
 
 
 app.listen(port, () => {
